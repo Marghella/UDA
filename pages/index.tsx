1b92fc13878014927eab5f875d979589d743511c
Sandro <75301022+Marghella@users.noreply.github.com>
2021-04-23 17:28:33 +0200

    aggiunto stile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import video from '../public/videos/landing.mp4';
+// import video from '../public/videos/landing.mp4';
+import { useSpring, animated } from 'react-spring';
 
 export default function Home() {
+  const [toggle, settoggle] = useState(false);
+  const styles = useSpring({ color: toggle ? 'red' : 'blue', fontSize: toggle ? '10px' : '72px' });
+  useEffect(() => {
+    setInterval(() => {
+      settoggle(!toggle);
+    }, 1000);
+  });
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Il mondo che vorrei</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <video src={video} style={{ width: '100vw', height: '100vh' }} autoPlay loop muted /> */}
-      <div>Vonazzo scema</div>
+      <animated.div style={styles}>Vonazzo scema</animated.div>
     </>
   );
 }
