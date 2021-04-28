c082392618a19894bf1c63ce1199c508e3586e13
Sandro <75301022+Marghella@users.noreply.github.com>
2021-04-28 17:56:05 +0200

    added clsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Head from 'next/head';
 // import video from '../public/videos/landing.mp4';
 import { useSpring, animated } from 'react-spring';
+import clsx from 'clsx';
 import {
   ForbiceDestraSVG, ForbiceSinistraSVG, LineaTraggiataSVG, NuvolaSVG, ScrittaSVG,
 } from '../public/images/logo/logo';
 import styles from '../styles/logotry.module.scss';
 
 export default function Home() {
+  const scissors = useRef(null);
   const [toggle, settoggle] = useState(false);
   const styless = useSpring({ color: toggle ? 'red' : 'blue', fontSize: toggle ? '10px' : '72px' });
   useEffect(() => {
-    const int = setInterval(() => {
-      settoggle(!toggle);
-    }, 1000);
+    const int = setTimeout(() => {
+      settoggle(true);
+    }, 700);
     return () => clearInterval(int);
   });
   return (
@@ -24,9 +26,10 @@ export default function Home() {
       </Head>
       {/* <video src={video} style={{ width: '100vw', height: '100vh' }} autoPlay loop muted /> */}
       <animated.div style={styless}>Vonazzo scema</animated.div>
-      <ForbiceDestraSVG className={styles.forbiceDestra} />
-      <ForbiceSinistraSVG className={styles.forbiceSinistra} />
+      <ForbiceDestraSVG className={styles.forbiceDestra} ref={scissors}/>
+      <ForbiceSinistraSVG className={clsx(styles.forbiceSinistra, toggle && styles.cut)} />
       <LineaTraggiataSVG className={styles.lineatratteggiata} />
+      <div className={styles.buildnuvola}/>
       <NuvolaSVG className={styles.nuvola}/>
       <ScrittaSVG className={styles.scritta}/>
     </>
